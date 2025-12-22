abb4865f4c248f59981ab68e03a0997ae6f868e9
Jims42 <jimmycoco.42@outlook.fr>
2025-12-22 12:41:27 +0100

    correction Benji

---
+++ Jalon_22_12_25/probleme_03_corrige_bberamis.java
@@ -0,0 +1,36 @@
+package Jalon_22_12_25;
+
+import java.util.Scanner;
+
+public class probleme_03_corrige_bberamis {
+    //suggestion de corrigé APRES EXAMEN
+    public static void main(String[] args) {
+
+
+    Scanner clavier = new Scanner(System.in);
+        int a;
+        int i = 0;
+
+        System.out.println("Combien voulez-vous compter en puissance de 2 ? ");
+        a = clavier.nextInt();
+
+        while (i <= a) {
+
+            double resultat = Math.pow(2, i);
+
+            System.out.println("2 puissance " + i + " = " + resultat);
+
+            i++;
+
+        }
+
+        /* for(int j = 0; j<=a; j++){
+            double resultat = Math.pow(2, j);
+
+            System.out.println("2 puissance " + j + " = " + resultat);
+
+        } */
+
+        clavier.close();
+    }
+}
